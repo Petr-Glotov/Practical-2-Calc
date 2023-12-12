5d45213d6edbc81ce90cb5b3677748ed2eaeecec
Petr Glotov <piter.glotov@yandex.ru>
2023-12-12 16:54:16 +0300

    Отправка на Github

---
+++ main.js
@@ -0,0 +1,39 @@
+let input = document.getElementById('input')
+let curInput = ""
+
+function addInput(value) {
+    curInput += value
+    input.value = curInput
+}
+
+function addInputClear() {
+    curInput = ""
+    input.value = ""
+}
+
+function appOper(operator) {
+    curInput += ' ' + operator + ' ';
+    input.value = curInput
+}
+
+function addInputResult() {
+    input.value = eval(curInput)
+    curInput = input.value
+}
+
+// function addInputResult() {
+//     const res = new Function ('return' + curInput)()
+//     input.value = res
+//     curInput = res
+// }
+
+// СОХРАНЕНИЕ В ПАМЯТЬ
+// let memory = 0
+// function addMemory() {
+//     memory += parseFloat(input.value) || 0
+//  }
+
+function retMemory() {
+    curInput += memory
+    input.value- curInput
+}
